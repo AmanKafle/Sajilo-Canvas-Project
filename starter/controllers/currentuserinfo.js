37ac0116641d2461b94b4263bf6abb226fa36763
AmanKafle <amankafleofficial@gmail.com>
2024-02-29 15:32:52 +0545

    /meAdded

--- starter/controllers/currentuserinfo.js
+++ starter/controllers/currentuserinfo.js
@@ -18,7 +18,7 @@ const currentUserInfo = async (req, res) => {
 
       }
     const freshUser = await User.findById(payload.id);
-    res.json(freshUser)
+    res.json({name:freshUser.name , role: freshUser.role, email:freshUser.email})
  
     } catch (error) {
       throw new UnauthenticatedError("Authentication invalid");
