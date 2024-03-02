ec700ad9ec1e983dc0447b945d7aa0e676af90cd
AmanKafle <amankafleofficial@gmail.com>
2024-03-02 20:32:17 +0545

    paymentAdded

--- starter/controllers/projectpage.js
+++ starter/controllers/projectpage.js
@@ -1,4 +1,6 @@
 const Projectinfo = require("../models/projectinfo")
+const util = require('util')
+
 
 const projectinfo = async (req, res) => {
     const project = await Projectinfo.create({ ...req.body,userid:req.user._id });
@@ -7,20 +9,30 @@ const projectinfo = async (req, res) => {
 }
 const projectupdate = async(req,res)=>{
     id = req.user._id
-    
-    
-    let projects = await Projectinfo.find({userid : id});
-    if (projects.progress === "Pending" | "Completed"){
-        projects = await Projectinfo.find({userid: id}, {rooms:{ editedurl :0}})
-    }
+    let projects1 = await Projectinfo.find({userid : id,$or:[{progress:"Pending"},{progress:"completed"}]},{rooms:{ editedurl :0}});
+    let projects2 = await Projectinfo.find({userid : id,progress:"Delivered"});
 
+    res.json([...projects1,...projects2])
+    console.log(projects1);
+    // console.log(projects);
+    // projects.forEach(async(project)   => {
+    //     console.log(project.progress);
+    //     if (project.progress == "Delivered"){
+    //         res.json({projects})
+    //     }
+
+    //     else if(project.progress == "Pending" || "completed" ){
+
+    //     }
    
     
-    res.json({projects})
-     
-    
-}
-
+    //     projects =await  Projectinfo.find({userid: id}, {rooms:{ editedurl :0}})
+    //     res.json(projects)
+        
+        
+        
+    //     })
+        }
 const forwardedinfo = async(req, res) =>{
     const forwarded = await Projectinfo.findOneAndUpdate( {_id: req.body.id }, { $set :{forwardedto : req.body.email}},{new : true})
       res.json({forwarded})
